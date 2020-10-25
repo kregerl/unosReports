979e9d2e59f84e7afb8b92256ef9fa42170a0386
kregerl <loucaskreger@gmail.com>
2020-10-25 15:40:21 -0400

    argparse added

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 #
 import sys
+import argparse
 import numpy as np
 import pandas as pd
 
@@ -18,13 +19,13 @@ def group(file_in, file_out):
     is_gift_order = frame['gift_order'] == False
 
     results = {
-    'Refunds': decrementing_pivot_table_creator(frame[is_refund].rename(
+    'Refunds': decreasing_pivot_table_creator(frame[is_refund].rename(
         columns={'shipping_region': 'State', 'financial_status' : 'Number of refunds'}), 'State', 'Number of refunds', 'count'),
 
-    'Reships': decrementing_pivot_table_creator(frame[is_reship].rename(
+    'Reships': decreasing_pivot_table_creator(frame[is_reship].rename(
         columns={'shipping_region': 'State', 'net_sales' : 'Number of reships'}), 'State', 'Number of reships', 'count'),
 
-    'Gift Shipments': decrementing_pivot_table_creator(frame[is_gift_order].rename(
+    'Gift Shipments': decreasing_pivot_table_creator(frame[is_gift_order].rename(
         columns={'shipping_region': 'State', 'gift_order' : 'Number of gift orders'}), 'State', 'Number of gift orders', 'count')
     }
 
@@ -32,7 +33,7 @@ def group(file_in, file_out):
 
 
 
-def decrementing_pivot_table_creator(filtered_frame, index, values, func):
+def decreasing_pivot_table_creator(filtered_frame, index, values, func):
     frame = filtered_frame.pivot_table(index = [index], values = [values], aggfunc = func, margins=True, margins_name='Total')
     tot_frame = pd.DataFrame(frame)[len(frame[values]) -1:].copy(deep = True)
     frame = frame.drop(['Total'], axis = 0).sort_values(by = values, ascending=False)
@@ -44,7 +45,24 @@ def save_sheets_to_workbook(results, file):
         for key in results:
             results[key].to_excel(writer, sheet_name=key)
     
+def handle_args():
+    parser = argparse.ArgumentParser(description='Creates reports with the given data exported from shopify')
+
+    parser.add_argument('-in', '-i', '-input', '--input',
+     dest='file_input', help='The shopify report used to generate other reports')
+     
+    parser.add_argument('-out', '-o', '-output', '--output',
+     dest='file_output', help='The output destination of the generated reports')
+
+    args = parser.parse_args()
+
+    return args
+
+
 if __name__ == '__main__':
-    path_in = "values.csv"
-    path_out = 'final.xlsx'
-    group(path_in, path_out)
+    args = handle_args()
+
+    if args.file_input and args.file_output:
+        group(args.file_input, args.file_output)
+    else:
+        print("Error: Unspecified file(s) please make sure there is a file name after the arguments.")
